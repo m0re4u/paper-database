37ba322b2b5676ca3caf48cb46930b0b9333a484
Michiel van der Meer <michiel@vandermeer.ai>
2020-07-20 12:22:14 +0200

    Exporting paper database at ma 20 jul 2020 12:22:14 CEST

--- export.sh
+++ export.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 jabref --import papers.bib --output exported/papers.md,simple_with_link --nogui
+
+git add .
+
+# Commit changes.
+msg="Exporting paper database at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
+
